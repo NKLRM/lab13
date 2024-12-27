f7c7df40a0d1d3fb2052b4c51fac8b7d90226782
NKLRM <nekoloveramenxd@gmail.com>
2024-12-27 14:33:09 +0700

    lab13_1 complete

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,20 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum=0,sum2=0,sum3=1,sum4=0;
+    B[4]=A[0]; B[5]=A[0];
+    for(int i=0;i<N;i++){
+        sum=sum+A[i];
+        sum2=sum2+pow(A[i],2);
+        sum3=sum3*A[i];
+        sum4=sum4+1/A[i];
+        if(B[4]<A[i]) B[4]=A[i];
+        if(B[5]>A[i]) B[5]=A[i];
+    }
+    B[0]=sum/N;
+    B[1]=sqrt(sum2/N-pow(B[0],2));
+    B[2]=pow(sum3,1.0/N);
+    B[3]=N/sum4;
+}
